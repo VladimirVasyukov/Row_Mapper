fb8fd5162388a9d5c7154eafa4ead1bb0bddbc33
Vladimir Vasyukov <vladimir.vasyukov7@gmail.com>
2021-11-17 22:44:20 +0300

    Employees. Row Mapper

---
+++ src/main/java/com/epam/rd/autocode/EmployeeRowMapper.java
@@ -0,0 +1,42 @@
+package com.epam.rd.autocode;
+
+import com.epam.rd.autocode.domain.Employee;
+import com.epam.rd.autocode.domain.FullName;
+import com.epam.rd.autocode.domain.Position;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeRowMapper implements RowMapper<Employee> {
+    private static final byte COLUMN_ID = 1;
+    private static final byte COLUMN_FIRST_NAME = 2;
+    private static final byte COLUMN_LAST_NAME = 3;
+    private static final byte COLUMN_MIDDLE_NAME = 4;
+    private static final byte COLUMN_POSITION = 5;
+    private static final byte COLUMN_HIREDATE = 7;
+    private static final byte COLUMN_SALARY = 8;
+
+    @Override
+    public Employee mapRow(ResultSet resultSet) {
+        List<Employee> employeeList = new ArrayList<>();
+        try {
+            BigInteger id = new BigInteger(String.valueOf(resultSet.getLong(COLUMN_ID)));
+            FullName fullName = new FullName(
+                resultSet.getString(COLUMN_FIRST_NAME),
+                resultSet.getString(COLUMN_LAST_NAME),
+                resultSet.getString(COLUMN_MIDDLE_NAME));
+            Position position = Position.valueOf(resultSet.getString(COLUMN_POSITION));
+            LocalDate hired = resultSet.getDate(COLUMN_HIREDATE).toLocalDate();
+            BigDecimal salary = resultSet.getBigDecimal(COLUMN_SALARY);
+            employeeList.add(new Employee(id, fullName, position, hired, salary));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employeeList.get(0);
+    }
+}
